955ee28ab1787b1630f64a0f7e4b94db43647cac
ramaariandi <ramaa104@gmail.com>
2021-03-24 23:24:32 +0700

    update join class

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,16 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
-  <title>Class</title>
+    <title>Welcome</title>
 
-  <style>
+    <style>
+     .container{
+        max-width: 1100px;
+    }
     .card {
       box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
       transition: 0.3s;
@@ -18,63 +19,63 @@
     .card:hover {
       box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
     }
-  </style>
+    </style>
 
-</head>
-<body>
+  </head>
+  <body>
 
-<nav class="navbar navbar-inverse bg-blue navbar-center">
-  <ul class="nav navbar-nav">
-    <a class="navbar-brand align-center"  href="#">
-				<strong>Invest Yuk!</strong>
-		</a>
-  </ul>
-</nav>
-
-&nbsp;
-&nbsp;
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <ul class="navbar-nav ">
+            <li class="nav-item ">
+                <a class="navbar-brand" href="#">Invest Yuk!</a>
+            </li>
+        </ul>
+    </nav>
 
 
+<div class="container">
+<br>
 <div class="card-group">
-  <div class="container">
-    <div class="row">
-      <div class="card">
-        <div class="container">
-          <h4><b>Kelas</b></h4> 
-            <p>Investasi Dunia</p> 
-        </div>
-      </div>
-      &nbsp;
-      <div class="card">
-        <div class="container">
-          <h4><b>Kelas</b></h4> 
-            <p>Investasi Dunia</p> 
-        </div>
-      </div>
-      &nbsp;
-      <div class="card">
-        <div class="container">
-          <h4><b>Kelas</b></h4> 
-            <p>Investasi Dunia</p> 
-        </div>
-      </div>
-
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
     </div>
   </div>
 </div>
+</div>
 
+<br>
 
+</div>
 
-
-
-
-
-
-
-
-
-
-
+<div class="card-group">
+  <div class="container">
+    <div class="row-grup">
+      <div class="card">
+        <div class="container">
+          <h4><b>Kelas</b></h4> 
+            <p>Investasi Dunia</p> 
+        </div>
+      </div>
+      &nbsp;
+      <div class="card">
+        <div class="container">
+          <h4><b>Kelas</b></h4> 
+            <p>Investasi Dunia</p> 
+        </div>
+      </div>
+      &nbsp;
+      <div class="card">
+        <div class="container">
+          <h4><b>Kelas</b></h4> 
+            <p>Investasi Dunia</p> 
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 </body>
 </html>
